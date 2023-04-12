1ceac85e3a834d7530cdfc89800a30b75ff586b7
lRadixl <bogdanpros19ua@gmail.com>
2023-04-12 20:06:06 +0300

    Lab 2

---
+++ lab2/lab2_2/ComplexArray.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2_2
+{
+    public class ComplexNumber
+    {
+        private double real;
+        private double imaginary;
+
+        public ComplexNumber(double real, double imaginary)
+        {
+            this.real = real;
+            this.imaginary = imaginary;
+        }
+
+        public ComplexNumber(double real) : this(real, 0) { }
+
+        public ComplexNumber() : this(0, 0) { }
+
+        public double Real
+        {
+            get { return real; }
+            set { real = value; }
+        }
+
+        public double Imaginary
+        {
+            get { return imaginary; }
+            set { imaginary = value; }
+        }
+
+        public static ComplexNumber I { get; } = new ComplexNumber(0, 1);
+
+        public static ComplexNumber Zero { get; } = new ComplexNumber(0, 0);
+
+        public static ComplexNumber operator +(ComplexNumber a, ComplexNumber b)
+        {
+            return new ComplexNumber(a.Real + b.Real, a.Imaginary + b.Imaginary);
+        }
+
+        public static ComplexNumber operator -(ComplexNumber a, ComplexNumber b)
+        {
+            return new ComplexNumber(a.Real - b.Real, a.Imaginary - b.Imaginary);
+        }
+
+        public static ComplexNumber operator *(ComplexNumber a, ComplexNumber b)
+        {
+            return new ComplexNumber(a.Real * b.Real - a.Imaginary * b.Imaginary, a.Real * b.Imaginary + a.Imaginary * b.Real);
+        }
+
+        public static ComplexNumber operator /(ComplexNumber a, ComplexNumber b)
+        {
+            double denominator = b.Real * b.Real + b.Imaginary * b.Imaginary;
+            return new ComplexNumber((a.Real * b.Real + a.Imaginary * b.Imaginary) / denominator, (a.Imaginary * b.Real - a.Real * b.Imaginary) / denominator);
+        }
+
+        public double Modulus()
+        {
+            return Math.Sqrt(real * real + imaginary * imaginary);
+        }
+
+        public ComplexNumber Power(int n)
+        {
+            ComplexNumber result = new ComplexNumber(1);
+            for (int i = 0; i < n; i++)
+            {
+                result *= this;
+            }
+            return result;
+        }
+
+        public static ComplexNumber[] SumOfNeighbors(ComplexNumber[] A)
+        {
+            ComplexNumber[] C = new ComplexNumber[A.Length - 1];
+            for (int i = 0; i < C.Length; i++)
+            {
+                double modulus = (A[i] + A[i + 1]).Modulus();
+                C[i] = new ComplexNumber(modulus, 0);
+            }
+            return C;
+        }
+
+        public override string ToString()
+        {
+            return $"{Real} + {Imaginary}i";
+        }
+    }
+}
---
+++ lab2/lab2_2/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab2_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Створення двох комплексних чисел і виконання деяких арифметичних операцій
+            ComplexNumber a = new ComplexNumber(3, 2);
+            ComplexNumber b = new ComplexNumber(-1, 4);
+            ComplexNumber c = a + b;
+            ComplexNumber d = a * b;
+            ComplexNumber f = a.Power(3);
+
+            // Виведіть результати
+            textBox1.Text = $"a = {a}\r\n";
+            textBox1.AppendText($"b = {b}\r\n");
+            textBox1.AppendText($"a + b = {c}\r\n");
+            textBox1.AppendText($"a * b = {d}\r\n");
+            textBox1.AppendText($"a^3 = {f}\r\n");
+
+            // Обчислення суми сусідніх комплексних чисел із вхідного масиву
+            ComplexNumber[] A = { new ComplexNumber(2, 3), new ComplexNumber(-1, 5), new ComplexNumber(0, -2) };
+            ComplexNumber[] C = ComplexNumber.SumOfNeighbors(A);
+
+            // Вивести результат
+            textBox2.Text = "Input array: ";
+            foreach (ComplexNumber num in A)
+            {
+                textBox2.AppendText(num + ", ");
+            }
+            textBox2.AppendText("\r\n");
+
+            textBox2.AppendText("Moduli of neighboring sums: ");
+            foreach (ComplexNumber num in C)
+            {
+                textBox2.AppendText(num.Modulus() + ", ");
+            }
+            textBox2.AppendText("\r\n");
+        }
+    }
+}
