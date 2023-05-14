aef6a99df158610c4ad7482022550d2695a18754
lRadixl <bogdanpros19ua@gmail.com>
2023-05-14 13:43:06 +0300

    lab9

---
+++ lab9/Lab9-2/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace Lab9_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBoxCoeff = new System.Windows.Forms.TextBox();
+            this.buttonDraw = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart.Legends.Add(legend2);
+            this.chart.Location = new System.Drawing.Point(41, 12);
+            this.chart.Name = "chart";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart.Series.Add(series2);
+            this.chart.Size = new System.Drawing.Size(492, 300);
+            this.chart.TabIndex = 0;
+            this.chart.Text = "chart";
+            // 
+            // textBoxCoeff
+            // 
+            this.textBoxCoeff.Location = new System.Drawing.Point(591, 43);
+            this.textBoxCoeff.Name = "textBoxCoeff";
+            this.textBoxCoeff.Size = new System.Drawing.Size(100, 20);
+            this.textBoxCoeff.TabIndex = 1;
+            // 
+            // buttonDraw
+            // 
+            this.buttonDraw.Location = new System.Drawing.Point(562, 140);
+            this.buttonDraw.Name = "buttonDraw";
+            this.buttonDraw.Size = new System.Drawing.Size(129, 55);
+            this.buttonDraw.TabIndex = 2;
+            this.buttonDraw.Text = "Побудувати";
+            this.buttonDraw.UseVisualStyleBackColor = true;
+            this.buttonDraw.Click += new System.EventHandler(this.buttonDraw_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonDraw);
+            this.Controls.Add(this.textBoxCoeff);
+            this.Controls.Add(this.chart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+        private System.Windows.Forms.TextBox textBoxCoeff;
+        private System.Windows.Forms.Button buttonDraw;
+    }
+}
+
---
+++ lab9/Lab9-2/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Lab9_2
+{
+    public partial class Form1 : Form
+    {
+        private double coeff; // Змінна для зберігання коефіцієнта
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Очищення всіх серій і областей діаграми
+            chart.Series.Clear();
+            chart.ChartAreas.Clear();
+
+            // Створення нової області діаграми
+            ChartArea area = new ChartArea("Area1");
+
+            // Встановлення мінімальних і максимальних значень для осей
+            area.AxisX.Minimum = -10;
+            area.AxisX.Maximum = 10;
+            area.AxisY.Minimum = -10;
+            area.AxisY.Maximum = 10;
+
+            // Встановлення назв осей
+            area.AxisX.Title = "X";
+            area.AxisY.Title = "Y";
+
+            // Додавання області до діаграми
+            chart.ChartAreas.Add(area);
+
+            // Створення нової серії даних
+            Series series = new Series("Function");
+
+            // Встановлення типу серії даних
+            series.ChartType = SeriesChartType.Line;
+
+            // Додавання серії до діаграми
+            chart.Series.Add(series);
+        }
+
+        private void buttonDraw_Click(object sender, EventArgs e)
+        {
+            // Перетворення значення з текстового поля в число і зберігання його в змінній coeff
+            double.TryParse(textBoxCoeff.Text, out coeff);
+
+            // Очищення всіх точок серії
+            chart.Series[0].Points.Clear();
+
+            // Розрахунок значень для серії даних
+            for (double t = -Math.PI; t <= Math.PI; t += 0.01)
+            {
+                // Розрахунок значень X і Y за допомогою параметричних рівнянь
+                double x = Math.Pow(t, 2) * Math.Sin(t);
+                double y = t * Math.Pow(Math.Cos(t), 2);
+
+                // Додавання точок до серії даних
+                chart.Series[0].Points.AddXY(x * coeff, y * coeff);
+            }
+        }
+    }
+}
