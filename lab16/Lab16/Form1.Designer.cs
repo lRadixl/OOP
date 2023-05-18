df38c49555e19dfb82a1f7c592ddbef67f312bed
lRadixl <bogdanpros19ua@gmail.com>
2023-05-18 23:50:59 +0300

    Lab16

---
+++ lab16/Lab16/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿
+namespace Lab16
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.userNameTextBox = new System.Windows.Forms.TextBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.logoutButton = new System.Windows.Forms.Button();
+            this.chatTextBox = new System.Windows.Forms.TextBox();
+            this.messageTextBox = new System.Windows.Forms.TextBox();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.chatSettingsButton = new System.Windows.Forms.Button();
+            this.serverAddressTextBox = new System.Windows.Forms.TextBox();
+            this.serverPortTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введіть ім\'я";
+            // 
+            // userNameTextBox
+            // 
+            this.userNameTextBox.Location = new System.Drawing.Point(104, 38);
+            this.userNameTextBox.Name = "userNameTextBox";
+            this.userNameTextBox.Size = new System.Drawing.Size(175, 20);
+            this.userNameTextBox.TabIndex = 1;
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(295, 38);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(106, 20);
+            this.loginButton.TabIndex = 2;
+            this.loginButton.Text = "Вхід";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // logoutButton
+            // 
+            this.logoutButton.Location = new System.Drawing.Point(295, 64);
+            this.logoutButton.Name = "logoutButton";
+            this.logoutButton.Size = new System.Drawing.Size(106, 20);
+            this.logoutButton.TabIndex = 3;
+            this.logoutButton.Text = "Вихід";
+            this.logoutButton.UseVisualStyleBackColor = true;
+            this.logoutButton.Click += new System.EventHandler(this.logoutButton_Click);
+            // 
+            // chatTextBox
+            // 
+            this.chatTextBox.Location = new System.Drawing.Point(35, 90);
+            this.chatTextBox.Multiline = true;
+            this.chatTextBox.Name = "chatTextBox";
+            this.chatTextBox.Size = new System.Drawing.Size(366, 179);
+            this.chatTextBox.TabIndex = 4;
+            // 
+            // messageTextBox
+            // 
+            this.messageTextBox.Location = new System.Drawing.Point(35, 286);
+            this.messageTextBox.Multiline = true;
+            this.messageTextBox.Name = "messageTextBox";
+            this.messageTextBox.Size = new System.Drawing.Size(259, 54);
+            this.messageTextBox.TabIndex = 5;
+            // 
+            // sendButton
+            // 
+            this.sendButton.Location = new System.Drawing.Point(300, 286);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(105, 54);
+            this.sendButton.TabIndex = 6;
+            this.sendButton.Text = "Надіслати";
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // chatSettingsButton
+            // 
+            this.chatSettingsButton.Location = new System.Drawing.Point(217, 379);
+            this.chatSettingsButton.Name = "chatSettingsButton";
+            this.chatSettingsButton.Size = new System.Drawing.Size(77, 36);
+            this.chatSettingsButton.TabIndex = 7;
+            this.chatSettingsButton.Text = "Встановити IP та Порт";
+            this.chatSettingsButton.UseVisualStyleBackColor = true;
+            this.chatSettingsButton.Click += new System.EventHandler(this.chatSettingsButton_Click);
+            // 
+            // serverAddressTextBox
+            // 
+            this.serverAddressTextBox.Location = new System.Drawing.Point(50, 388);
+            this.serverAddressTextBox.Name = "serverAddressTextBox";
+            this.serverAddressTextBox.Size = new System.Drawing.Size(100, 20);
+            this.serverAddressTextBox.TabIndex = 8;
+            // 
+            // serverPortTextBox
+            // 
+            this.serverPortTextBox.Location = new System.Drawing.Point(156, 388);
+            this.serverPortTextBox.Name = "serverPortTextBox";
+            this.serverPortTextBox.Size = new System.Drawing.Size(46, 20);
+            this.serverPortTextBox.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(300, 379);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(77, 36);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "SaveChat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 456);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.serverPortTextBox);
+            this.Controls.Add(this.serverAddressTextBox);
+            this.Controls.Add(this.chatSettingsButton);
+            this.Controls.Add(this.sendButton);
+            this.Controls.Add(this.messageTextBox);
+            this.Controls.Add(this.chatTextBox);
+            this.Controls.Add(this.logoutButton);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.userNameTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox userNameTextBox;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Button logoutButton;
+        private System.Windows.Forms.TextBox chatTextBox;
+        private System.Windows.Forms.TextBox messageTextBox;
+        private System.Windows.Forms.Button sendButton;
+        private System.Windows.Forms.Button chatSettingsButton;
+        private System.Windows.Forms.TextBox serverAddressTextBox;
+        private System.Windows.Forms.TextBox serverPortTextBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
