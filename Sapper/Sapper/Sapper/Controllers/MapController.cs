c9a93ac4e1ab2f30dc51121adf1eed89e77ef079
lRadixl <bogdanpros19ua@gmail.com>
2023-05-28 13:22:56 +0300

    -

---
+++ Sapper/Sapper/Sapper/Controllers/MapController.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sapper
+{
+    public static class MapController
+    {
+        public const int mapSize = 8; // Розмір карти
+        public const int cellSize = 50; // Розмір комірки
+
+        private static int currentPictureToSet = 0; // Поточний індекс спрайту для встановлення
+
+        public static int[,] map = new int[mapSize, mapSize]; // Масив для зберігання даних про карту
+
+        public static Button[,] buttons = new Button[mapSize, mapSize]; // Масив кнопок для представлення карті
+
+        public static Image spriteSet; // Зображення спрайтів
+
+        private static bool isFirstStep; // Прапорець першого кроку
+
+        private static Point firstCoord; // Перша координата
+
+        public static Form form; // Форма
+
+        private static void ConfigureMapSize(Form current)
+        {
+            // Конфігурація розміру форми залежно від розміру карти
+            current.Width = mapSize * cellSize + 20;
+            current.Height = (mapSize + 1) * cellSize;
+        }
+
+        private static void InitMap()
+        {
+            // Ініціалізація карти, всі значення встановлюються на 0
+            for (int i = 0; i < mapSize; i++)
+            {
+                for (int j = 0; j < mapSize; j++)
+                {
+                    map[i, j] = 0;
+                }
+            }
+        }
+
+        public static void Init(Form current)
+        {
+            // Ініціалізація контролера карти
+            form = current;
+            currentPictureToSet = 0;
+            isFirstStep = true;
+            spriteSet = new Bitmap(Path.Combine(new DirectoryInfo(Directory.GetCurrentDirectory()).Parent.Parent.FullName.ToString(), "Sprites\\tiles.png")); // Завантаження спрайтів з файлу
+            ConfigureMapSize(current);
+            InitMap();
+            InitButtons(current);
+        }
+
+        private static void InitButtons(Form current)
+        {
+            // Ініціалізація кнопок на формі
+            for (int i = 0; i < mapSize; i++)
+            {
+                for (int j = 0; j < mapSize; j++)
+                {
+                    Button button = new Button();
+                    button.Location = new Point(j * cellSize, i * cellSize);
+                    button.Size = new Size(cellSize, cellSize);
+                    button.Image = FindNeededImage(0, 0);
+                    button.MouseUp += new MouseEventHandler(OnButtonPressedMouse);
+                    current.Controls.Add(button);
+                    buttons[i, j] = button;
+                }
+            }
+        }
+
+        private static void OnButtonPressedMouse(object sender, MouseEventArgs e)
+        {
+            // Обробка події натискання кнопки миші
+            Button pressedButton = sender as Button;
+            switch (e.Button.ToString())
+            {
+                case "Right":
+                    OnRightButtonPressed(pressedButton);
+                    break;
+                case "Left":
+                    OnLeftButtonPressed(pressedButton);
+                    break;
+            }
+        }
+
+        private static void OnRightButtonPressed(Button pressedButton)
+        {
+            // Обробка натискання правої кнопки миші
+            currentPictureToSet++;
+            currentPictureToSet %= 3;
+            int posX = 0;
+            int posY = 0;
+            switch (currentPictureToSet)
+            {
+                case 0:
+                    posX = 0;
+                    posY = 0;
+                    break;
+                case 1:
+                    posX = 0;
+                    posY = 2;
+                    break;
+                case 2:
+                    posX = 2;
+                    posY = 2;
+                    break;
+            }
+            pressedButton.Image = FindNeededImage(posX, posY);
+        }
+
+        private static void OnLeftButtonPressed(Button pressedButton)
+        {
+            // Обробка натискання лівої кнопки миші
+            pressedButton.Enabled = false;
+            int iButton = pressedButton.Location.Y / cellSize;
+            int jButton = pressedButton.Location.X / cellSize;
+            if (isFirstStep)
+            {
+                firstCoord = new Point(jButton, iButton);
+                SeedMap();
+                CountCellBomb();
+                isFirstStep = false;
+            }
+            OpenCells(iButton, jButton);
+
+            if (map[iButton, jButton] == -1)
+            {
+                ShowAllBombs(iButton, jButton);
+                MessageBox.Show("Вы програли" +
+                    "");
+                form.Controls.Clear();
+                Init(form);
+            }
+        }
+
+        private static void ShowAllBombs(int iBomb, int jBomb)
+        {
+            // Показати всі бомби на карті після поразки
+            for (int i = 0; i < mapSize; i++)
+            {
+                for (int j = 0; j < mapSize; j++)
+                {
+                    if (i == iBomb && j == jBomb)
+                        continue;
+                    if (map[i, j] == -1)
+                    {
+                        buttons[i, j].Image = FindNeededImage(3, 2);
+                    }
+                }
+            }
+        }
+
+        public static Image FindNeededImage(int xPos, int yPos)
+        {
+            // Знайти необхідний спрайт залежно від позиції
+            Image image = new Bitmap(cellSize, cellSize);
+            Graphics g = Graphics.FromImage(image);
+            g.DrawImage(spriteSet, new Rectangle(new Point(0, 0), new Size(cellSize, cellSize)), 0 + 32 * xPos, 0 + 32 * yPos, 33, 33, GraphicsUnit.Pixel);
+
+
+            return image;
+        }
+
+        private static void SeedMap()
+        {
+            // Заповнення карти випадковими бомбами
+            Random r = new Random();
+            int number = r.Next(7, 15);
+
+            for (int i = 0; i < number; i++)
+            {
+                int posI = r.Next(0, mapSize - 1);
+                int posJ = r.Next(0, mapSize - 1);
+
+                while (map[posI, posJ] == -1 || (Math.Abs(posI - firstCoord.Y) <= 1 && Math.Abs(posJ - firstCoord.X) <= 1))
+                {
+                    posI = r.Next(0, mapSize - 1);
+                    posJ = r.Next(0, mapSize - 1);
+                }
+                map[posI, posJ] = -1;
+            }
+        }
+
+        private static void CountCellBomb()
+        {
+            // Підрахунок кількості бомб біля кожної комірки на карті
+            for (int i = 0; i < mapSize; i++)
+            {
+                for (int j = 0; j < mapSize; j++)
+                {
+                    if (map[i, j] == -1)
+                    {
+                        for (int k = i - 1; k < i + 2; k++)
+                        {
+                            for (int l = j - 1; l < j + 2; l++)
+                            {
+                                if (!IsInBorder(k, l) || map[k, l] == -1)
+                                    continue;
+                                map[k, l] = map[k, l] + 1;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void OpenCell(int i, int j)
+        {
+            // Відкриття комірки на карті
+            buttons[i, j].Enabled = false;
+
+            switch (map[i, j])
+            {
+                case 1:
+                    buttons[i, j].Image = FindNeededImage(1, 0);
+                    break;
+                case 2:
+                    buttons[i, j].Image = FindNeededImage(2, 0);
+                    break;
+                case 3:
+                    buttons[i, j].Image = FindNeededImage(3, 0);
+                    break;
+                case 4:
+                    buttons[i, j].Image = FindNeededImage(4, 0);
+                    break;
+                case 5:
+                    buttons[i, j].Image = FindNeededImage(0, 1);
+                    break;
+                case 6:
+                    buttons[i, j].Image = FindNeededImage(1, 1);
+                    break;
+                case 7:
+                    buttons[i, j].Image = FindNeededImage(2, 1);
+                    break;
+                case 8:
+                    buttons[i, j].Image = FindNeededImage(3, 1);
+                    break;
+                case -1:
+                    buttons[i, j].Image = FindNeededImage(1, 2);
+                    break;
+                case 0:
+                    buttons[i, j].Image = FindNeededImage(0, 0);
+                    break;
+            }
+        }
+
+        private static void OpenCells(int i, int j)
+        {
+            // Рекурсивне відкриття комірок на карті
+            OpenCell(i, j);
+
+            if (map[i, j] > 0)
+                return;
+
+            for (int k = i - 1; k < i + 2; k++)
+            {
+                for (int l = j - 1; l < j + 2; l++)
+                {
+                    if (!IsInBorder(k, l))
+                        continue;
+                    if (!buttons[k, l].Enabled)
+                        continue;
+                    if (map[k, l] == 0)
+                        OpenCells(k, l);
+                    else if (map[k, l] > 0)
+                        OpenCell(k, l);
+                }
+            }
+        }
+
+        private static bool IsInBorder(int i, int j)
+        {
+            // Перевірка, чи координати комірки знаходяться в межах карті
+            if (i < 0 || j < 0 || i >= mapSize || j >= mapSize)
+                return false;
+            return true;
+        }
+    }
+}
+
---
+++ Sapper/Sapper/Sapper/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("Sapper")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Sapper")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("e402848b-4c28-4c56-b523-39e5daa95ae4")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
